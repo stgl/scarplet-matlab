6a891988c88e5e47b6a86a05165c25f9422bc3e6
rmsare <robertmsare@gmail.com>
2015-07-21 08:34:29 -0700

    comments and cleanup

--- calcprofcurv.m
+++ calcprofcurv.m
@@ -10,37 +10,20 @@ function c = calcprofcurv(dem,de,gam)
 %%
 %% OUTPUT:      c - curvature grid 
 
-%x = (1:length(dem(1,:))).*de;
-%y = (1:length(dem(:,1))).*de;
-
-%x = x - mean(x);
-%y = y - mean(y);
-
-%[X,Y] = meshgrid(x,y);
-
+% Profile orientation
 alph = (gam).*pi./180;
 
-%Xrot = cos(alph).*X - sin(alph).*Y;
-%Yrot = sin(alph).*X + cos(alph).*Y;
-
-%Z = interp2(X,Y,dem,Xrot,Yrot,'linear');
-
-%dZdx = diff(Z,1,2)./de;
-%c = diff(dZdx,1,2)./de;
-
-%c = [zeros(length(dZdx(:,1)),1) c zeros(length(dZdx(:,1)),1)];
-
+% First- and second-order finite differences
 dzdx = diff(dem,1,2)./de;
 d2zdxdy = diff(dzdx,1,1)./de;
-
 d2zdxdy = [zeros(length(d2zdxdy(:,1)),1) d2zdxdy];
 d2zdxdy = [zeros(1,length(d2zdxdy(1,:)));d2zdxdy];
 d2zdx2 = diff(dem,2,2)./de.^2;
 d2zdx2 = [zeros(length(d2zdx2(:,1)),1) d2zdx2 zeros(length(d2zdx2(:,1)),1)];
-
 d2zdy2 = diff(dem,2,1)./de.^2;
 d2zdy2 = [zeros(1,length(d2zdy2(1,:)));d2zdy2;zeros(1,length(d2zdy2(1,:)))];
 
 %[dzdx,dzdy] = gradient(dem,de);
 
+% Second-order finite difference approximation to DEM curvature
 c = d2zdx2.*(cos(alph)).^2 + d2zdy2.*(sin(alph)).^2 - 2.*d2zdxdy.*(sin(alph)*cos(alph));
--- wavelet_scarp.m
+++ wavelet_scarp.m
@@ -16,22 +16,26 @@ function [W,M,anx,any] = wavelet_scarp(frac,d,gam,kt,x,y)
 %%              anx - horizontal window limit in x direction
 %%              any - horizontal window limit in y direction
 
+% Limits of scarp curvature
 c = abs(erfinv(frac) .* 2.*sqrt(kt));
 
+% Set up template grid 
 [X,Y] = meshgrid(x,y);
 
 alph = -gam.*pi./180;
 Xrot = X.*cos(alph)+ Y.*sin(alph);
 Yrot = -X.*sin(alph) + Y.*cos(alph);
 
+% Curvature of template scarp
 W = -Xrot./(2.*kt.^(3/2).*pi.^(1/2)) .* (exp(-Xrot.^2./(4.*kt)));
 M = W;
 M(:) = 1;
 
-
+% Mask template to 0 if we are outside scarp bounds
 W = W.*((abs(Xrot) < c) & (abs(Yrot) < d));
 M = M.*((abs(Xrot) < c) & (abs(Yrot) < d));
 
+% Template limits, used for clipping
 x4 = d.*cos(alph-pi./2);
 y4 = d.*sin(alph-pi./2);
 x1 = d.*cos(alph);
