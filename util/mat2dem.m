444206d0591b9e4219be85bbe16a6f030ee4a5b3
rmsare <robertmsare@gmail.com>
2015-07-22 14:36:35 -0700

    wrote export utility

--- util/mat2dem.m
+++ util/mat2dem.m
@@ -1,33 +1,28 @@
-function dem = dem2mat(filename)
+function [] = mat2dem(dem, filename)
 
-%% Load ESRI ASCII DEM as MATLAB structure
+%% Save MATLAB DEM as ESRI ASCII grid
 %% Robert Sare 2014
 %%
-%% INPUT:       filename - ASCII file containing DEM
-%%
-%% OUTPUT:      dem - dem in matlab grid struct
-
-% Define DEM structure
-dem = struct('nx', 0, 'ny', 0, 'xllcenter', 0, 'yllcenter', 0, 'de', 0, 'grid', [], 'nodata', NaN);
+%% INPUT:       dem - dem to save
+%%              filename - ASCII file 
 
 % Load DEM info
-fid = fopen(filename);
-C = textscan(fid,'%s %f',6);
+fid = fopen(filename, 'w');
 
-% Get DEM dimension and resolution
-dem.nx = C{2}(1);
-dem.ny = C{2}(2);
-dem.de = C{2}(5);
+% Write grid parameters to file
+fprintf(fid, 'ncols %i\n', dem.nx);
+fprintf(fid, 'nrows %i\n', dem.ny);
+fprintf(fid, 'xllcenter %10.3f\n', dem.xllcenter);
+fprintf(fid, 'yllcenter %10.3f\n', dem.yllcenter);
+fprintf(fid, 'cellsize %8.3f\n', dem.de);
+fprintf(fid, 'nodata_value %10.3f\n', dem.nodata);
 
-% Get DEM corner in UTM coordinates
-dem.xllcenter = C{2}(3);
-%dem.xllcenter = x1+(dem.nx/2)*dem.de;
-dem.yllcenter = C{2}(4);
-%dem.yllcenter = y1+(dem.ny/2)*dem.de;
+idx = find(isnan(dem.grid));
+dem.grid(idx) = dem.nodata;
 
-% Load DEM grid
-ndv = C{2}(6);
-%dem.nodata = ndv; % retain NDVs; useful for ESRI compatibility
-dem.grid(dem.grid==ndv) = NaN; % set NaNs; useful for matlab processing
-dem.grid = fscanf(fid,'%f',[dem.nx,dem.ny]);
-dem.grid = flipud(dem.grid')
+for(i = dem.ny:-1:1)
+    fprintf(fid, '%10.3f', dem.grid(i,:));
+    fprintf(fid, '\n');
+end
+
+fclose(fid);
