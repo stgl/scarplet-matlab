84cb64925882955e1a7704e9ed9b1e98f120c571
rmsare <robertmsare@gmail.com>
2015-07-22 14:36:07 -0700

    wrote carrizo example

---
+++ examples/carrizo.m
@@ -0,0 +1,23 @@
+% wavelet analysis of Carrizo Plain DEM
+% Robert Sare June 2015
+
+% Import DEM -- Carrizo Plain, 2m resolution
+dem = dem2mat('carrizo.asc');
+
+% Filter DEM at 200m template length
+d = 200;
+[A, KT, ANG, SNR] = wavelet_filtertile(dem, d);
+
+% Plot raw results
+%plotscarplet(dem, SNR);
+plotscarplet(dem, KT);
+
+% Simple mask by SNR
+mask = SNR.grid >= mean(SNR.grid(:)));
+KT.grid = log(KT.grid);
+KT.grid(~mask) = nan;
+
+plotscarplet(dem, KT);
+
+% Save as ESRI ASCII files
+mat2dem(KT, 'carrizo_kt_200m.asc');
