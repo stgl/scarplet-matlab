9359d09ba6f392e25d32b180b1d761d9adb5b73f
rmsare <robertmsare@gmail.com>
2015-07-22 15:46:58 -0700

    added mean option and nan message

--- util/noisedem.m
+++ util/noisedem.m
@@ -1,10 +1,11 @@
-function [noisedem, nanidx] = noisedem(dem, sig2)
+function [noisedem, nanidx] = noisedem(dem, sig2, meanval)
 
 %% Whitens DEM with Gaussian noise around mean value
 %% Robert Sare 2014
 %%
 %% INPUT:       dem - DEM struct to whiten
 %%              sig2 - noise variance
+%%              meanval - noise mean
 %% OUTPUT:      noisedem - whitened DEM
 %%              nanidx - linear indices of NaN values, useful for
 %%                       post-processing
@@ -15,10 +16,15 @@ end
 
 tot = sum(nansum(dem.grid));
 num = sum(sum(~isnan(dem.grid)));
-meanval = tot/num;
+if nargin < 3
+    meanval = tot/num;
+end
+
 noisegrid = randn(dem.ny,dem.nx)*sig2 + meanval;
 
 idx = find(isnan(dem.grid));
+fprintf('DEM contains %i NaNs, (%i %%)', length(idx),
+length(idx)/prod(size(dem.grid))*100);
 dem.grid(idx) = noisegrid(idx);
 
 noisedem = dem;
