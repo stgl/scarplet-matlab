b96392e8ced85d3bf6304ed402bbd6f4c487f684
rmsare <robertmsare@gmail.com>
2015-07-22 16:17:03 -0700

    wrote plotting utility

--- util/plotscarplet.m
+++ util/plotscarplet.m
@@ -1,33 +1,65 @@
-function dem = dem2mat(filename)
+function [] = plotscarplet(dem, param)
 
-%% Load ESRI ASCII DEM as MATLAB structure
+%% Plot hillshade and parameter overlay
 %% Robert Sare 2014
+%% Incorporates code written to produce figures in Hilley, et al., 2010 GRL
+%% paper
 %%
-%% INPUT:       filename - ASCII file containing DEM
-%%
-%% OUTPUT:      dem - dem in matlab grid struct
+%% INPUT:       dem - dem in matlab grid struct
+%%              param - grid of parameters
 
-% Define DEM structure
-dem = struct('nx', 0, 'ny', 0, 'xllcenter', 0, 'yllcenter', 0, 'de', 0, 'grid', [], 'nodata', NaN);
+figure
+hold on
 
-% Load DEM info
-fid = fopen(filename);
-C = textscan(fid,'%s %f',6);
+% Plot hillshade (need slopemag and slopeaz to do this)
+I = ploths(dem, 315, 5);
 
-% Get DEM dimension and resolution
-dem.nx = C{2}(1);
-dem.ny = C{2}(2);
-dem.de = C{2}(5);
+% Plot parameter grid
+cmap = jet(254);
+cmap = [1 1 1; cmap];
+hsv_cmap = rgb2hsv(cmap);
 
-% Get DEM corner in UTM coordinates
-dem.xllcenter = C{2}(3);
-%dem.xllcenter = x1+(dem.nx/2)*dem.de;
-dem.yllcenter = C{2}(4);
-%dem.yllcenter = y1+(dem.ny/2)*dem.de;
+pmin = nanmin(param.grid);
+pmax = nanmax(param.grid);
 
-% Load DEM grid
-ndv = C{2}(6);
-%dem.nodata = ndv; % retain NDVs; useful for ESRI compatibility
-dem.grid(dem.grid==ndv) = NaN; % set NaNs; useful for matlab processing
-dem.grid = fscanf(fid,'%f',[dem.nx,dem.ny]);
-dem.grid = flipud(dem.grid')
+% Rescale to colormap
+colorgrid = (param.grid - pmin)./(pmax - pmin);
+idx = param.grid == param.nodata | isnan(param.grid);
+colorgrid(idx) = 0;
+colorgrid(colorgrid > 1) = 0;
+
+x = (param.xllcenter+param.de./2):param.de:((param.nx).*param.de + (param.xllcenter-param.de./2));
+y = (param.yllcenter+param.de./2):param.de:((param.ny).*param.de + (param.yllcenter-param.de./2));
+
+colorimage(:, :, 1) = reshape(hsv_cmap(round(C.*254)+1, 1), param.ny, param.nx);
+colorimage(:, :, 2) = reshape(hsv_cmap(round(C.*254)+1, 2), param.ny, param.nx);
+colorimage(:, :, 3) = I;
+
+colorimage = hsv2rgb(colorimage);
+
+colormap gray
+image(x, y, colorimage);
+
+xlabel('Easting');
+ylabel('Northing');
+
+a = gca;set(a,'ydir','normal');
+axis equal;axis image;
+
+% -----------------------------------------------------------------------------
+% Internal functions
+function I = ploths(dem,az,elev)
+
+az = -az .* pi ./ 180;
+elev = elev .* pi ./ 180;
+
+I = cos(elev).*sin(dem.slopemag).*cos(dem.slopeaz-az) + sin(elev).*cos(dem.slopemag);
+I = (I + 1)./2 .* 255;
+
+x = (dem.xllcenter+dem.de./2):dem.de:((dem.nx).*dem.de + (dem.xllcenter-dem.de./2));
+y = (dem.yllcenter+dem.de./2):dem.de:((dem.ny).*dem.de + (dem.yllcenter-dem.de./2));
+
+colormap gray
+imagesc(x,y,I,[27 216]);
+
+I = I./255;
